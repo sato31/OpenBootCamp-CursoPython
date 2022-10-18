3958d839068a50caf79ad9144db5f407b224c6e7
Oscar <sanchez.torres.oscar.a@gmail.com>
2022-10-18 17:49:52 -0500

    Añadido el ejercicio del tema 2 Interprete de Python

---
+++ 02_InterpreteDePython/VariableModificada.py
@@ -0,0 +1,9 @@
+# Modifica la variable del anterior ejercicio en la consola de python
+# y después muestrala por consola para ver la modificación de la variable.
+
+helloWorld = "Hola Mundo!"
+print(helloWorld)
+# "Hola Mundo!"
+helloWorld = "Soy un Hola Mundo Modificado"
+print(helloWorld)
+# "Soy un Hola Mundo Modificado"
